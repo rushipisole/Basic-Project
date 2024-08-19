cc0ff3280fc6944b31c45e25eec97f174a3645e4
Rushi Pisole <rushikeshpisole3@gmail.com>
2024-08-19 14:20:36 +0530

    Succfully Created the web application using nodejs and express

--- Backend/index.js
+++ Backend/index.js
@@ -51,8 +51,10 @@ const path = require('path');
 const app = express();
 const port = 8080;
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override")
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -88,7 +90,8 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content })
+    let id = uuidv4();
+    posts.push({ id, username, content })
     res.redirect("/posts")
 })
 
@@ -96,6 +99,27 @@ app.get("/posts/:id", (req, res) => {
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("show.ejs", { post })
+})
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent
+    console.log(post);
+    res.redirect("/posts")
+})
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post })
+})
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts")
 })
 
 app.listen(port, () => {
